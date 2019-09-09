7ecc9ac7223d8cc82decb4a25d11a0b9770ea5fc
okjihoon <dhrwlgns13@gmail.com>
2019-09-09 15:26:25 +0900

    dbcp

--- funweb/src/com/exam/dao/DBManager.java
+++ funweb/src/com/exam/dao/DBManager.java
@@ -2,64 +2,83 @@ package com.exam.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class DBManager {
+
 	public static Connection getConnection() throws Exception {
+		Connection con = null;
+		/*
 		// DB접속정보
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String user = "scott";
 		String password = "tiger";
-		
-		Connection con = null;
+
 		// 1단계: DB 드라이버 로딩
 		Class.forName("oracle.jdbc.OracleDriver");
 		// 2단계: DB연결
 		con = DriverManager.getConnection(url, user, password);
+		*/
+		
+		// DBCP 적용한 코드
+		// DataSource는 커넥션을 미리 일정갯수 보유하고 있음
+		Context context = new InitialContext();
+		DataSource ds = (DataSource) context.lookup("java:/comp/env/jdbc/oracledb");
+		con = ds.getConnection(); // 커넥션 한개 빌려오기
 		return con;
 	}
 	
-	// SELECT문을 수행한 후 리소스 해제를 위한 메소드 
+	// SELECT문을 수행한 후 리소스 해제를 위한 메소드
 	public static void close(Connection con, Statement stmt, ResultSet rs) {
-		try {
-			if (rs != null ) {
-				rs.close();	
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-		} catch(Exception e)	{
-			e.printStackTrace();
 		}
-		try {
-			if (stmt != null ) {
-				stmt.close();	
+		if (stmt != null) {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-		} catch(Exception e)	{
-			e.printStackTrace();
 		}
-		try {
-			if (con != null ) {
-				con.close();	
+		if (con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-		} catch(Exception e)	{
-			e.printStackTrace();
 		}
-	}
+	} // close method
+	
 	// INSERT, UPDATE, DELETE문을 수행한 후 리소스 해제를 위한 메소드
-		public static void close(Connection con, Statement stmt) {
-			if (stmt != null) {
-				try {
-					stmt.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
+	public static void close(Connection con, Statement stmt) {
+		if (stmt != null) {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-			if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
+		}
+		if (con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-		} // close method
+		}
+	} // close method
+	
 }
+
+
+
